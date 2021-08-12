1bfc38e218823924b07ea07671d6fd53a567e064
DeveloperRooney <han_0427@naver.com>
2021-08-12 10:02:11 +0900

    Spring Security 연습 프로젝트 생성

---
+++ src/main/java/com/sh/springsecexam/SpringsecexamApplication.java
@@ -0,0 +1,13 @@
+package com.sh.springsecexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecexamApplication.class, args);
+    }
+
+}
