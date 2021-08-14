366c7222edba0f52ed9b5e4104487973eed6fcfe
DeveloperRooney <han_0427@naver.com>
2021-08-14 15:18:15 +0900

    Config 수정

--- src/main/java/com/sh/springsecexam/config/SecurityConfig.java
+++ src/main/java/com/sh/springsecexam/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sh.springsecexam.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,7 +10,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+
+                .formLogin()
+                .loginPage("/user/login")
+                .permitAll()
+                .loginProcessingUrl("/user/auth")
+                .defaultSuccessUrl("/")
+
+                .and()
+
+                .logout()
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
+
     }
 }
